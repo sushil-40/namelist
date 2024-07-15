761c4c917b90950616036eee33eb1e8a326db13c
Sushil <grayson.9999@gmail.com>
2024-07-15 19:35:03 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { getByDisplayValue } from "@testing-library/react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="wrapper"
+      style={{
+        display: "flex",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "2rem",
+      }}
+    >
+      <div
+        className="userList"
+        style={{ boxShadow: "0 0 10px grey", padding: "2rem" }}
+      >
+        <div className="display">As you are Typing</div>
+        <div className="form">
+          <form action="">
+            <input type="text" />
+            <button>Add User</button>
+          </form>
+        </div>
+        <hr />
+        <div className="list">
+          <ul>
+            <li>Tej</li>
+            <li>Suni</li>
+            <li>Satya</li>
+            <li>Zarina</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
