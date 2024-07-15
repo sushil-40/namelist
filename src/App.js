853da637a9f6f62b5a86e76db4d0d7dd104083f2
Sushil <grayson.9999@gmail.com>
2024-07-15 19:56:16 +1000

    Implementing real time display of inputed items

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import { getByDisplayValue } from "@testing-library/react";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  //if we need any update in value then should use state  (like useState)
+  const [name, setName] = useState("Tej");
+
+  const handleOnChange = (e) => {
+    // const str = e.target.value;
+    // console.log(str);
+
+    // target is object so that we can destructure and use like below
+    const { value } = e.target;
+
+    //update destructured name property of useState
+    setName(value);
+    // console.log(value);
+  };
   return (
     <div
       className="wrapper"
@@ -17,10 +32,15 @@ function App() {
         className="userList"
         style={{ boxShadow: "0 0 10px grey", padding: "2rem" }}
       >
-        <div className="display">As you are Typing</div>
+        <div className="display">{name}</div>
         <div className="form">
           <form action="">
-            <input type="text" />
+            <input
+              type="text"
+              required
+              placeholder="enter name"
+              onChange={handleOnChange}
+            />
             <button>Add User</button>
           </form>
         </div>
